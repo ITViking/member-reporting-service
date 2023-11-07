c71a92f1cfcfbd7bfd152b330cea38ae56937624
Philip <philip@deranged.dk>
2023-11-07 20:38:40 +0100

    make port configurable

--- src/start.ts
+++ src/start.ts
@@ -6,14 +6,14 @@ import config from 'config';
 (async function() {
   const mongoClient = new MongoClient(config.get("mongodb.url"));
   await mongoClient.connect();
-  console.log("do we get this far?")
   const db:Db = mongoClient.db(config.get("mongodb.db"));
 
   const server = await app(db);
   let service;
   smacker.start({
       start: () => {
-        service = server.listen(5000, '0.0.0.0')
+        service = server.listen(config.get('port'), '0.0.0.0');
+        console.log(`Server is running on port ${config.get('port')}`);
       },
       stop: () => service.close()
   });
